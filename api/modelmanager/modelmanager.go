db300e4da16aa1494fd5921982617357c4b48533
Menno Smits <menno.smits@canonical.com>
2017-02-22 21:13:55 +1300

    Added `juju add-caas-model` command

--- api/modelmanager/modelmanager.go
+++ api/modelmanager/modelmanager.go
@@ -378,7 +378,8 @@ func (c *Client) UnsetModelDefaults(cloud, region string, keys ...string) error 
 
 // CreateCAASModel creates a new CAAS model.
 func (c *Client) CreateCAASModel(
-	name, owner, endpoint, certData, keyData, caData string,
+	name, owner, endpoint string,
+	certData, keyData, caData []byte,
 ) (string, error) {
 	if !names.IsValidUser(owner) {
 		return "", errors.Errorf("invalid owner name %q", owner)
@@ -388,9 +389,9 @@ func (c *Client) CreateCAASModel(
 		Name:     name,
 		OwnerTag: names.NewUserTag(owner).String(),
 		Endpoint: endpoint,
-		CertData: []byte(certData),
-		KeyData:  []byte(keyData),
-		CAData:   []byte(caData),
+		CertData: certData,
+		KeyData:  keyData,
+		CAData:   caData,
 	}
 	var modelInfo params.CAASModelInfo
 	err := c.facade.FacadeCall("CreateCAASModel", createArgs, &modelInfo)
