1615879814d16601504d2fb0303ea0123081e21c
Menno Smits <menno.smits@canonical.com>
2017-04-19 12:20:18 +1200

    worker/caasprovisioner: Better handling of configmap
    Only create the ConfigMap if it doesn't already exist. Moved more
    Juju specific bits from k8s.go to worker.go.

--- worker/caasprovisioner/worker.go
+++ worker/caasprovisioner/worker.go
@@ -6,8 +6,13 @@ package caasprovisioner
 import (
 	"github.com/juju/errors"
 	"github.com/juju/loggo"
+	"gopkg.in/juju/names.v2"
 	"gopkg.in/juju/worker.v1"
 
+	"github.com/juju/juju/agent"
+	"github.com/juju/juju/network"
+	"github.com/juju/juju/state"
+	"github.com/juju/juju/version"
 	"github.com/juju/juju/worker/caasmodelworkermanager"
 	"github.com/juju/juju/worker/catacomb"
 )
@@ -58,6 +63,10 @@ func (p *provisioner) loop() error {
 		return errors.Annotate(err, "creating k8s client")
 	}
 
+	newConfig := func(appName string) ([]byte, error) {
+		return newOperatorConfig(appName, st)
+	}
+
 	// XXX this loop should also keep an eye on kubernetes and ensure
 	// that the operator stays up, redeploying it if the pod goes
 	// away. For some runtimes we *could* rely on the the runtime's
@@ -69,7 +78,7 @@ func (p *provisioner) loop() error {
 		case apps := <-w.Changes():
 			for _, app := range apps {
 				logger.Infof("saw app: %s", app)
-				if err := ensureOperator(client, app, st); err != nil {
+				if err := ensureOperator(client, app, newConfig); err != nil {
 					// XXX need retry logic rather than just giving up
 					// (see queue concept in storage provisioner)
 					logger.Errorf("ensure failed: %v", err)
@@ -80,4 +89,66 @@ func (p *provisioner) loop() error {
 			return p.catacomb.ErrDying()
 		}
 	}
+}
+
+func newOperatorConfig(appName string, st *state.CAASState) ([]byte, error) {
+	appTag := names.NewApplicationTag(appName)
+
+	apiAddrs, err := apiAddresses(st)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	controllerCfg, err := st.ControllerConfig()
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	caCert, ok := controllerCfg.CACert()
+	if !ok {
+		return nil, errors.New("missing ca cert in controller config")
+	}
+
+	conf, err := agent.NewAgentConfig(
+		agent.AgentConfigParams{
+			Paths: agent.Paths{
+				// XXX shouldn't be hardcoded
+				DataDir: "/var/lib/juju",
+				LogDir:  "/var/log/juju",
+			},
+			// This isn't actually used but needs to be supplied.
+			UpgradedToVersion: version.Current,
+			Tag:               appTag,
+			Password:          "XXX not currently checked",
+			Controller:        st.ControllerTag(),
+			Model:             st.ModelTag(),
+			APIAddresses:      apiAddrs,
+			CACert:            caCert,
+		},
+	)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	confBytes, err := conf.Render()
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return confBytes, nil
+}
+
+func apiAddresses(st *state.CAASState) ([]string, error) {
+	apiHostPorts, err := st.APIHostPorts()
+	if err != nil {
+		return nil, err
+	}
+	var addrs = make([]string, 0, len(apiHostPorts))
+	for _, hostPorts := range apiHostPorts {
+		ordered := network.PrioritizeInternalHostPorts(hostPorts, false)
+		for _, addr := range ordered {
+			if addr != "" {
+				addrs = append(addrs, addr)
+			}
+		}
+	}
+	return addrs, nil
 }
