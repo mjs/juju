789cf5e38df6326b08a1b96fbaf4539b6b478545
Joel Sing <joel.sing@canonical.com>
2017-04-20 17:19:03 +1000

    Pass correct type for CAAS application deployment and specify num units.

--- cmd/juju/application/deploy.go
+++ cmd/juju/application/deploy.go
@@ -657,7 +657,6 @@ func (c *DeployCommand) deployCharm(
 		return errors.Trace(apiRoot.CAASDeploy(caasapplication.DeployArgs{
 			CharmID:         id,
 			ApplicationName: serviceName,
-			Series:          series,
 			NumUnits:        numUnits,
 			ConfigYAML:      string(configYAML),
 		}))
